0f146a1bf7efbd8fd2b9bb776b1197b96dad0ec7
Linsley Michira <linsleymichira@outlook.com>
2026-05-11 20:44:27 -0400

    Remove deprecated models and update schema to reflect new Snowflake data sources

---
+++ dv2_public_data_demo/models/staging/v_stg_irs_form990_loans_and_notes.sql
@@ -0,0 +1,25 @@
+{{ config(materialized="view") }}
+
+{%- set yaml_metadata -%}
+
+source_model:
+    SNOWFLAKE_PUBLIC_DATA_FREE: "IRS_FORM990_LOANS_AND_NOTES"
+
+derived_columns:
+
+hashed_columns:
+
+{%- endset -%}
+
+{% set metadata_dict = fromyaml(yaml_metadata) %}
+
+{{
+    automate_dv.stage(
+        include_source_columns=true,
+        source_model=metadata_dict["source_model"],
+        derived_columns=metadata_dict["derived_columns"],
+        null_columns=none,
+        hashed_columns=metadata_dict["hashed_columns"],
+        ranked_columns=none,
+    )
+}}
